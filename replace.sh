6d69587e41d0637ad73879beb539ac147fae7e13
diademiemi <emilia@diademiemi.me>
2023-05-09 12:17:32 +0200

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -9,12 +9,8 @@ fi
 
 if [ -z "$2" ]; then
     if [ -z "$GITHUB_USER" ]; then
-        if [ "$USERNAME" == "diademiemi" ]; then
-            GITHUB_USER="diademiemi" # For my own convenience
-        else
-            echo "Enter github user: "
-            read GITHUB_USER
-        fi
+        echo "Enter github user: "
+        read GITHUB_USER
     fi
 else
     GITHUB_USER="$2"
@@ -25,12 +21,12 @@ if [ -z "$GALAXY_API_KEY" ]; then
     read GALAXY_API_KEY
 fi
 
+# Assumes repo is named ansible_collection_${GITHUB_USER}.${NEW_ROLE_NAME}
+gh secret set GALAXY_API_KEY -R ${GITHUB_USER}/ansible_collection_${GITHUB_USER}.${NEW_COLLECTION_NAME} -a actions -b ${GALAXY_API_KEY}
 
 find roles docs meta plugins galaxy.yml LICENSE README.md \
     -type f -exec sed -i -e "s/diademiemi/${GITHUB_USER}/g" -e "s/template/${NEW_COLLECTION_NAME}/g" {} + # Do not run this more than once
 
-# Assumes repo is named ansible_role_${NEW_ROLE_NAME}
-gh secret set GALAXY_API_KEY -R ${GITHUB_USER}/ansible_collection_${GITHUB_USER}.${NEW_COLLECTION_NAME} -a actions -b ${GALAXY_API_KEY}
 
 # Remove this section from README.md
 sed -i "/Collection Structure/Q" README.md
