6f6a33cb71c7b326b27a6630dfcf4d000ec1caef
diademiemi <emilia@diademiemi.me>
2023-10-28 20:28:47 +0200

    Add workflow on adding role

--- add-role.sh
+++ add-role.sh
@@ -7,6 +7,8 @@ mkdir -p ./roles/$role_name
 
 wget -O /tmp/ansible_role_template.$$ https://github.com/diademiemi/ansible_role_template/archive/refs/heads/main.zip 
 
+./.github/workflow-templates/copy-role-template.sh $role_name
+
 cd ./roles/$role_name
 
 ln -s . ansible_role_template-main
@@ -16,3 +18,5 @@ rm ansible_role_template-main
 rm /tmp/ansible_role_template.$$
 
 ROLE_IN_COLLECTION="true" bash replace.sh $role_name
+
+echo "Role $role_name created"
